6bbc451bb3f0fbe60023381002c47318b25540e4
Raoben <99082352+Raoben@users.noreply.github.com>
2022-09-23 14:25:14 +0200

    Yaboi1

--- GUI.py
+++ GUI.py
@@ -1,4 +1,3 @@
-
 from doctest import master
 from fileinput import filename
 import tkinter as tk
@@ -9,24 +8,30 @@ from tkinter import *
 from tkinter.filedialog import askopenfilenames
 
 root = tk.Tk()
-from os import walk
+
 #This is Frame with AudiRed
-canvas = tk.Canvas(root, height=600, width=600, bg="#b51c14")
+canvas = tk.Canvas(root, height=500, width=500, bg="#b51c14")
 canvas.pack()
 
 #this is white templet
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
- 
+
+# folder path
+dir_path = r'C:\Users\robin\Documents\Plocklistor'
+# list to store files name
+def lof():
+    ext = ('pdf')
+    for files in os.listdir(dir_path):
+        if files.endswith(ext):
+            for file in files:
+                my_button = tk.Button(frame, text=files, padx=50, pady=5, fg="black", bg="#FFFFFF",)
+            my_button.pack()
+
 
 #Button UPDATE LIST
 PrintPPG = tk.Button(root, text="UPDATE LIST", padx=10, pady=5,
-                          fg="white", bg="#8C00E6",)
+                          fg="white", bg="#8C00E6",command=lof)
 PrintPPG.pack() 
 
-#Youtube Filedialog code
-master = ()
-root.filename = filedialog.askopenfilenames(initialdir="/Plocklistor", title="Select A File", filetypes=(("pdf files", "*.pdf"),("all files", "*.*")))
-my_label = Label(frame, text=root.filename).pack()
-
-root.mainloop() 
+root.mainloop()
